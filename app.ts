a094b1e7d00930c23b7e194e6c3e3008e7b97c85
Chisom <chisomije92@gmail.com>
2022-07-02 16:14:59 +0100

    resolve cors header

--- app.ts
+++ app.ts
@@ -5,7 +5,15 @@ import feedRoutes from "./routes/feed";
 const app = express();
 
 // app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 
 app.use("/feed", feedRoutes);
--- dist/app.js
+++ dist/app.js
@@ -8,6 +8,12 @@ const feed_1 = __importDefault(require("./routes/feed"));
 // import bodyParser from "body-parser";
 const app = (0, express_1.default)();
 // app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 app.use(express_1.default.json());
 app.use("/feed", feed_1.default);
 app.listen(8080);
